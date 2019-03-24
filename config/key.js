a75b75c8ed8bcaa7745ea7226d5ef38ba6ac5dad
thaominh123 <duynguyenquykhanh@gmail.com>
2019-03-24 01:37:59 -0700

    Updated server.js

---
+++ config/key.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://duyn:meyeu2000@ds049864.mlab.com:49864/hackiot"
+};
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/key.js").mongoURI;
+const bodyParse = require("body-parser");
+const morgan = require("morgan");
+
+//MiddleWare
+app.use(bodyParse.urlencoded({ extended: false }));
+app.use(bodyParse.json());
+app.use(morgan("dev"));
+
+//Prevent CORS And Allow PUT,POST,DELETE,PATCH,GET
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"),
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, DELETE, PATCH, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+mongoose
+  .connect(db)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log("Running Server at " + port));
